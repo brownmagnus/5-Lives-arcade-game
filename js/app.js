6a08f8b3beeb054975fe35b97375105716336e15
Brownmagnus Olivers <brownmagnuso@gmail.com>
2018-08-02 12:43:01 -0400

    Move enemy across the screen

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 var Enemy = function() {
     this.x = 101;
     this.y = 83;
-
+    this.speed = 150;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +14,9 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // Move forward across the screen
+    this.x += this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
